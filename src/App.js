e0947613c603c063fb839c301b81548ce55acc60
Jonathan <jonathan3692bf@users.noreply.github.com>
2021-01-18 00:13:41 +0100

    * Add messaging for how many adjustments remain * Make sure Leaderboard stays consistent * Add some text explaining the nature of this "game"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { v4 as uuid } from 'uuid'
 import { ProgressSpinner } from 'primereact/progressspinner'
 import { InputText } from 'primereact/inputtext'
 import { DataTable } from 'primereact/datatable'
@@ -10,6 +11,7 @@ const MAX_CLICK_COUNT = 10
 
 function App () {
   const [loading, setLoading] = useState(true)
+  const [id, setId] = useState(uuid())
   const [name, setName] = useState('')
   const [totalPoints, setTotalPoints] = useState(0)
   const [clickCount, setClickCount] = useState(0)
@@ -22,31 +24,51 @@ function App () {
     async function requestScores () {
       setLoading(true)
       const latestScores = await api.getScores()
-      const latestScoresWithAveragePointsPerClick = latestScores
-        .sort((a, b) => {
-          return a.totalPoints - b.totalPoints
-        })
-        .slice(0, 10)
-        .map(score => {
-          score.avgPointsPerClick = Number(score.totalPoints / score.clicks).toFixed(2)
-          return score
-        })
-      setScores(latestScoresWithAveragePointsPerClick)
+      updateScores(latestScores)
       setLoading(false)
     }
     requestScores()
   }, [])
 
   function resetState () {
+    setId(uuid())
     setName('')
     setTotalPoints(0)
     setClickCount(0)
   }
   function adjustScore () {
-    if (clickCount < MAX_CLICK_COUNT) {
-      setClickCount(clickCount + 1)
-      setTotalPoints(totalPoints + Math.round((Math.random() * 200)) - 100)
+    if (!name) {
+      return setNameError(true)
     }
+    if (clickCount < MAX_CLICK_COUNT) {
+      const clicks = clickCount + 1
+      const newTotalPoints = totalPoints + Math.round((Math.random() * 200)) - 100
+      const updatedScores = scores.filter(score => {
+        return score.id !== id
+      })
+        .concat([{
+          id,
+          name,
+          clicks,
+          totalPoints: newTotalPoints,
+          avgPointsPerClick: Number(newTotalPoints / clicks).toFixed(2)
+        }])
+      setClickCount(clicks)
+      setTotalPoints(newTotalPoints)
+      updateScores(updatedScores)
+    }
+  }
+  function updateScores (scores) {
+    const updatedScores = scores
+      .sort((a, b) => {
+        return a.totalPoints - b.totalPoints
+      })
+      .slice(0, 10)
+      .map(score => {
+        score.avgPointsPerClick = Number(score.totalPoints / score.clicks).toFixed(2)
+        return score
+      })
+    setScores(updatedScores)
   }
   async function submitScores () {
     if (!name) {
@@ -60,47 +82,55 @@ function App () {
     }
     setRequestPending(true)
     await api.submitScore(submittedScore)
-    setRequestPending(false)
-    const updatedScores = scores.slice()
+    const updatedScores = scores.slice().filter(score => {
+      return score.id !== id
+    })
     updatedScores.push(submittedScore)
-    setScores(updatedScores)
+    updateScores(updatedScores)
+    setRequestPending(false)
     resetState()
   }
 
   return <div style={{ margin: '2rem' }}>
     <h1>High Score App</h1>
-    <br/>
+
     {loading
       ? <>
-      <div style={{ margin: 'auto' }}><ProgressSpinner className="p-d-block" /></div>
-      <h3 style={{ textAlign: 'center' }}>Getting scores</h3>
-    </>
+        <div style={{ margin: 'auto' }}><ProgressSpinner className="p-d-block" /></div>
+        <h3 style={{ textAlign: 'center' }}>Getting scores</h3>
+      </>
       : <>
-    <div className="p-grid">
-      <div className="p-col-fixed">
-        <div className={'p-float-label'}>
-          <InputText className={nameError ? 'p-invalid' : ''} id="name" value={name} onChange={(e) => {
-            setName(e.target.value)
-            setNameError(false)
-          }} />
-          <label htmlFor="name">Name</label>
+      <div>Enter a name and click "Adjust score" until you have a score that you are happy with.</div>
+      <div>You can adjust your score up to {MAX_CLICK_COUNT} times, but may chose to click "Save" at any point.</div>
+      <br/>
+      <div className="p-grid">
+        <div className="p-col-fixed">
+          <div className={'p-float-label'}>
+            <InputText className={nameError ? 'p-invalid' : ''} id="name" value={name} onChange={(e) => {
+              setName(e.target.value)
+              setNameError(false)
+            }} />
+            <label htmlFor="name">Name</label>
+          </div>
+          {nameError && <div className="p-invalid">
+            Name is required
+          </div>}
         </div>
-        {nameError && <div className="p-invalid">
-          Name is required
-        </div>}
+        <div className="p-col-fixed">
+          <Button label="Adjust score" disabled={disableAdjustButton || requestPending} className={`p-button-outlined ${disableAdjustButton ? 'p-button-secondary' : ''}`} onClick={adjustScore}/>
+        </div>
+        <div className="p-col-fixed">
+          <Button label="Save" className="p-button" onClick={submitScores} loading={requestPending}/>
+        </div>
       </div>
-      <div className="p-col-fixed">
-        <Button label="Adjust score" disabled={disableAdjustButton || requestPending} className={`p-button-outlined ${disableAdjustButton ? 'p-button-secondary' : ''}`} onClick={adjustScore}/>
-      </div>
-      <div className="p-col-fixed">
-        <Button label="Submit" className="p-button" onClick={submitScores} loading={requestPending}/>
-      </div>
-    </div>
-    <h3>Current score: {totalPoints}</h3>
+      <h3>Current score: {totalPoints} | Remaining adjustments: {MAX_CLICK_COUNT - clickCount}</h3>
+      {clickCount === MAX_CLICK_COUNT && <div>
+        You are out of adjustments. Click the "Save" button above to try again.
+      </div>}
     </>}
     {scores.length > 0 && <>
-      <h2>Leader board</h2>
-      <DataTable value={scores} sortField="totalPoints" sortOrder={-1}>
+      <h2>Leaderboard</h2>
+      <DataTable value={scores} loading={requestPending} sortField="totalPoints" sortOrder={-1}>
         <Column field="name" header="Name" sortable/>
         <Column field="totalPoints" header="Score"sortable/>
         <Column field="clicks" header="Clicks" sortable/>
--- src/lib/api.js
+++ src/lib/api.js
@@ -24,9 +24,20 @@ async function getScores () {
     const promise = new Promise(resolve => {
       setTimeout(() => {
         resolve([
-          { name: 'Jane Doe', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 10)) },
-          { name: 'Lily Allen', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 10)) },
-          { name: 'John Smith', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 10)) }
+          { id: 1, name: 'Jane Doe', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 9)) + 1 },
+          { id: 2, name: 'Lily Allen', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 9)) + 1 },
+          { id: 3, name: 'John Smith', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 9)) + 1 },
+          { id: 4, name: 'Will Smith', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 9)) + 1 },
+          { id: 5, name: 'Jayden Smith', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 9)) + 1 },
+          { id: 6, name: 'Willow Smith', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 9)) + 1 },
+          { id: 7, name: 'Adam Smith', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 9)) + 1 },
+          { id: 9, name: 'Ada Lovelace', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 9)) + 1 },
+          { id: 10, name: 'Nathan Smith', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 9)) + 1 },
+          { id: 11, name: 'Maynard Smith', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 9)) + 1 },
+          { id: 12, name: 'Leaky Smith', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 9)) + 1 },
+          { id: 13, name: 'Nightsmoke Smith', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 9)) + 1 },
+          { id: 14, name: 'Madhan Smith', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 9)) + 1 },
+          { id: 15, name: 'Jake Smith', totalPoints: Math.round((Math.random() * 2000)) - 1000, clicks: Math.round((Math.random() * 9)) + 1 }
         ])
       }, 1000)
     })
